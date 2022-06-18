2e1ff0e18c48d28237fb4883efe3b74f4a0e6b74
4qwerty1 <prosin270901@gmail.com>
2022-06-19 06:47:12 +1000

    task 20

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import bridge from '@vkontakte/vk-bridge';
-import { View, ScreenSpinner, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol } from '@vkontakte/vkui';
+import { View, ScreenSpinner, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol, ModalRoot, ModalCard } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import './static/global.css'
 
 import Home from './panels/Home';
 import Locations from './panels/Locations';
+import Game from './panels/Game';
 
 const App = () => {
 	const [scheme, setScheme] = useState('bright_light')
 	const [activePanel, setActivePanel] = useState('home');
+	const [activeModal, setActiveModal] = useState('');
+	const [location, setLocation] = useState('');
 
 	useEffect(() => {
 		bridge.subscribe(({ detail: { type, data }}) => {
@@ -23,15 +26,26 @@ const App = () => {
 		setActivePanel(e.currentTarget.dataset.to);
 	};
 
+	const openLocationModal = (location) => {
+		setActiveModal('location');
+		setLocation(location);
+	}
+
+	const modal = (
+		<ModalRoot activeModal={activeModal}>
+		  <ModalCard onClose={() => setActiveModal('')} id="location">{location}</ModalCard>
+		</ModalRoot>)
+
 	return (
 		<ConfigProvider scheme={scheme}>
 			<AdaptivityProvider>
 				<AppRoot>
-					<SplitLayout>
+					<SplitLayout modal={modal}>
 						<SplitCol>
 							<View activePanel={activePanel}>
 								<Locations id='locations' go={go} />
 								<Home id='home'  go={go} />
+								<Game id='game' go={go} openLocationModal={openLocationModal}/>
 							</View>
 						</SplitCol>
 					</SplitLayout>
--- src/panels/Game.js
+++ src/panels/Game.js
@@ -1,31 +1,171 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar, PanelHeaderBack, List } from '@vkontakte/vkui';
+import {
+  Panel,
+  PanelHeader,
+  PanelHeaderBack,
+  Group,
+  Header,
+  FormLayout,
+  FormItem,
+  Input,
+  Button,
+  Div,
+  Paragraph,
+  ButtonGroup,
+} from "@vkontakte/vkui";
+
 import { LOCATIONS } from '../static';
 
-const Game = ({ id, go }) => (
+const Game = ({ id, go, openLocationModal }) => {
+
+  const [countOfPlayers, setCountOfPlayers] = useState(3);
+  const changeCountOfPlayers = (evt) => {
+    setCountOfPlayers(evt.currentTarget.value);
+  }
+
+  const [countOfSpyes, setCountOfSpyes] = useState(1);
+  const changeCountOfSpyes = (evt) => {
+    setCountOfSpyes(evt.currentTarget.value);
+  }
+
+  const [gameStatus, setGameStatus] = useState({
+    gameStart: false,
+    dealStart: false,
+  })
+  
+  const [currentPlayer, setCurrentPlayer] = useState(1);
+  const [location, setLocation] = useState("");
+  const [spyUsers, setSpyUsers] = useState([]);
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  const dealStart = () => {
+    setGameStatus({
+        ...gameStatus,
+        dealStart: true,
+    })
+    for (let i = 1; i <= countOfSpyes; i += 1) {
+        setSpyUsers(spyUsers.concat([getRandomInt(0, countOfPlayers)]));
+    }
+    setLocation(
+        LOCATIONS[Object.keys(LOCATIONS)[getRandomInt(0, Object.keys(LOCATIONS).length)]]
+    );
+  }; 
+
+  const startGame = () => {
+    setGameStatus({
+        gameStart: true,
+        dealStart: false,
+    })
+  };
+
+  const nextPlayer = () => {
+    setCurrentPlayer(currentPlayer + 1);
+  };
+
+  const getSpyesStatus = () => {
+    if (countOfPlayers == 12) {
+      return countOfSpyes == 2 ? "valid" : "error";
+    } else {
+      return countOfSpyes >= 1 && countOfSpyes <= 2 ? "valid" : "error";
+    }
+  };
+
+  return (
     <Panel id={id}>
-        <PanelHeader>
-            Игра
-        </PanelHeader>
+      <PanelHeader left={<PanelHeaderBack onClick={go} data-to="home" />}>
+        Игра
+      </PanelHeader>
+
+      {!gameStatus.dealStart && !gameStatus.gameStart && (
         <Group>
-            <List>
-                {LOCATIONS && Object.keys(LOCATIONS).map(loc => {
-                    return (
-                        <Cell expandable key={loc}>
-                            {LOCATIONS[loc]}
-                        </Cell>
-                    )
-                })}
-            </List>
+          <Header>Подготовка</Header>
+          <FormLayout>
+            <FormItem
+              top="Количество игроков"
+              status={
+                countOfPlayers >= 3 && countOfPlayers <= 12 ? "valid" : "error"
+              }
+            >
+              <Input
+                type="number"
+                name="countOfPlayers"
+                value={countOfPlayers}
+                onChange={changeCountOfPlayers}
+              />
+            </FormItem>
+            <FormItem
+              top="Количество шпионов"
+              status={getSpyesStatus()}
+            >
+              <Input
+                type="number"
+                name="countOfSpyes"
+                value={countOfSpyes}
+                onChange={changeCountOfSpyes}
+              />
+            </FormItem>
+            <FormItem>
+              <Button size="l" stretched onClick={dealStart}>
+                Начать раздачу
+              </Button>
+            </FormItem>
+          </FormLayout>
         </Group>
+      )}
+      {gameStatus.dealStart && (
+        <Div style={{ textAlign: 'center'}}>
+          <Paragraph  style={{display: 'block', marginBottom: 10}}>
+            Игрок {currentPlayer}, ознакомтесь с локацией.
+          </Paragraph>
+          <ButtonGroup stretched mode="vertical">
+            <Button
+              stretched
+              mode="secondary"
+              size="m"
+              onClick={() => openLocationModal(spyUsers.includes(currentPlayer)
+                ? "Вы шпион"
+                : location)
+              }
+            >
+              Показать локацию
+            </Button>
+            {currentPlayer < countOfPlayers && (
+              <Button
+                stretched
+                mode="secondary"
+                size="m"
+                onClick={nextPlayer}
+              >
+                Следующий игрок
+              </Button>
+            )}
+            {currentPlayer == countOfPlayers && (
+              <Button stretched mode="secondary" size="m" onClick={startGame}>
+                Начать игру
+              </Button>
+            )}  
+          </ButtonGroup>
+        </Div>
+      )}
+      {gameStatus.gameStart && (
+        <Div style={{ display: 'flex', justifyContent: 'center'}}>
+          <Paragraph>Игра началась!</Paragraph>
+        </Div>
+      )}
     </Panel>
-);
+  );
+};
 
 Game.propTypes = {
-    id: PropTypes.string.isRequired,
-    go: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  go: PropTypes.func.isRequired,
 };
 
 export default Game;
